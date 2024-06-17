a9ddb3ed1d9b805ea67c4f373b857f7144ecab34
MAN B <80488842+ManB-crypto@users.noreply.github.com>
2024-06-17 16:37:30 +0800

    Update post.service.ts

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -6,13 +6,11 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class UniversityService {
-  private corsAnywhereUrl = 'https://cors-anywhere.herokuapp.com/';
-  private targetUrl = 'http://universities.hipolabs.com/search?country=United+States';
   private url = `${this.corsAnywhereUrl}${this.targetUrl}`;
 
   constructor(private httpClient: HttpClient) { }
 
   getUniversities(): Observable<any> {
-    return this.httpClient.get<any>(this.url);
+    return this.http.get('/api');
   }
 }
